7bf0c5b2ecd55e70f4ddfaf2451d68c52f568ba2
Daniel Egger <dan.egg@outlook.com>
2021-05-18 14:54:16 +0200

    Add onCLick method to MapActivity

--- app/src/main/java/at/aau/risiko/MapActivity.java
+++ app/src/main/java/at/aau/risiko/MapActivity.java
@@ -2,7 +2,10 @@ package at.aau.risiko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    public void onClick(View view) {
+        view.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ffffbb33")));
     }
 }
