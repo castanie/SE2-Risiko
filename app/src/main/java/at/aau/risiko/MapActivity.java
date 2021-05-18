1b4eab8ab2e18f58306473c859e58165bfcdb9d0
Daniel Egger <dan.egg@outlook.com>
2021-05-18 15:26:15 +0200

    Made Country buttons clickable

--- app/src/main/java/at/aau/risiko/MapActivity.java
+++ app/src/main/java/at/aau/risiko/MapActivity.java
@@ -7,15 +7,22 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
+import at.aau.risiko.core.Game;
+import at.aau.risiko.core.Player;
+
 public class MapActivity extends AppCompatActivity {
+
+    Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        game = new Game(new Player[3]);
     }
 
     public void onClick(View view) {
-        view.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ffffbb33")));
+        game.handleInput(view);
     }
 }
