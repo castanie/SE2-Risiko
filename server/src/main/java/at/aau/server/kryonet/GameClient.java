3c2703d6f661f58730e0170355788e72d4606cb9
Daniel Egger <dan.egg@outlook.com>
2021-06-16 16:02:05 +0200

    Added ConqueredMessage to notify Players to draw a card; moved message registration

--- server/src/main/java/at/aau/server/kryonet/GameClient.java
+++ server/src/main/java/at/aau/server/kryonet/GameClient.java
@@ -5,8 +5,23 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import at.aau.server.dto.BaseMessage;
+import at.aau.server.dto.CardMessage;
+import at.aau.server.dto.CheatedMessage;
+import at.aau.server.dto.CloseDiceActivitiesMessage;
+import at.aau.server.dto.DiceMessage;
+import at.aau.server.dto.ExchangeMessage;
+import at.aau.server.dto.EyeNumbersMessage;
+import at.aau.server.dto.LogMessage;
+import at.aau.server.dto.NameMessage;
+import at.aau.server.dto.ReadyMessage;
+import at.aau.server.dto.RequestPlayerMessage;
+import at.aau.server.dto.ResponsePlayerMessage;
+import at.aau.server.dto.StartMessage;
+import at.aau.server.dto.TurnMessage;
+import at.aau.server.dto.UpdateMessage;
 
 public class GameClient implements NetworkClient, KryoNetComponent {
     private static GameClient instance;
@@ -21,6 +36,26 @@ public class GameClient implements NetworkClient, KryoNetComponent {
         if (instance == null) {
             instance = new GameClient();
         }
+
+        instance.registerClass(int[].class);
+        instance.registerClass(Integer[].class);
+        instance.registerClass(String[].class);
+        instance.registerClass(ArrayList.class);
+        instance.registerClass(LogMessage.class);
+        instance.registerClass(NameMessage.class);
+        instance.registerClass(RequestPlayerMessage.class);
+        instance.registerClass(ResponsePlayerMessage.class);
+        instance.registerClass(StartMessage.class);
+        instance.registerClass(ReadyMessage.class);
+        instance.registerClass(TurnMessage.class);
+        instance.registerClass(UpdateMessage.class);
+        instance.registerClass(DiceMessage.class);
+        instance.registerClass(EyeNumbersMessage.class);
+        instance.registerClass(CheatedMessage.class);
+        instance.registerClass(CardMessage.class);
+        instance.registerClass(ExchangeMessage.class);
+        instance.registerClass(CloseDiceActivitiesMessage.class);
+
         return instance;
     }
 
--- server/src/main/java/at/aau/server/kryonet/GameServer.java
+++ server/src/main/java/at/aau/server/kryonet/GameServer.java
@@ -5,8 +5,23 @@ import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import at.aau.server.dto.BaseMessage;
+import at.aau.server.dto.CardMessage;
+import at.aau.server.dto.CheatedMessage;
+import at.aau.server.dto.CloseDiceActivitiesMessage;
+import at.aau.server.dto.DiceMessage;
+import at.aau.server.dto.ExchangeMessage;
+import at.aau.server.dto.EyeNumbersMessage;
+import at.aau.server.dto.LogMessage;
+import at.aau.server.dto.NameMessage;
+import at.aau.server.dto.ReadyMessage;
+import at.aau.server.dto.RequestPlayerMessage;
+import at.aau.server.dto.ResponsePlayerMessage;
+import at.aau.server.dto.StartMessage;
+import at.aau.server.dto.TurnMessage;
+import at.aau.server.dto.UpdateMessage;
 
 public class GameServer implements NetworkServer, KryoNetComponent {
     private static GameServer instance;
@@ -21,6 +36,26 @@ public class GameServer implements NetworkServer, KryoNetComponent {
         if (instance == null) {
             instance = new GameServer();
         }
+
+        instance.registerClass(int[].class);
+        instance.registerClass(Integer[].class);
+        instance.registerClass(String[].class);
+        instance.registerClass(ArrayList.class);
+        instance.registerClass(LogMessage.class);
+        instance.registerClass(NameMessage.class);
+        instance.registerClass(RequestPlayerMessage.class);
+        instance.registerClass(ResponsePlayerMessage.class);
+        instance.registerClass(StartMessage.class);
+        instance.registerClass(ReadyMessage.class);
+        instance.registerClass(TurnMessage.class);
+        instance.registerClass(UpdateMessage.class);
+        instance.registerClass(DiceMessage.class);
+        instance.registerClass(EyeNumbersMessage.class);
+        instance.registerClass(CheatedMessage.class);
+        instance.registerClass(CardMessage.class);
+        instance.registerClass(ExchangeMessage.class);
+        instance.registerClass(CloseDiceActivitiesMessage.class);
+
         return instance;
     }
 
