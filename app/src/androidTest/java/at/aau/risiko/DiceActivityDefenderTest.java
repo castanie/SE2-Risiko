5821bcb55b784630feebfc83428b638ad827cb0b
Raphael Lesacher <80222395+raphilesacher@users.noreply.github.com>
2021-07-15 11:56:41 +0200

    created UI tests for DiceActivityDefender

---
+++ app/src/androidTest/java/at/aau/risiko/DiceActivityDefenderTest.java
@@ -0,0 +1,50 @@
+package at.aau.risiko;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceActivityDefenderTest {
+
+    @Rule
+    public ActivityTestRule<DiceActivityDefender> rule = new ActivityTestRule<DiceActivityDefender>(DiceActivityDefender.class);
+
+    private DiceActivityDefender mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = rule.getActivity();
+    }
+
+    @Test
+    public void testLaunch() {
+        View dice1 = mActivity.findViewById(R.id.diceOneAttack);
+        assertNotNull(dice1);
+        View dice2 = mActivity.findViewById(R.id.diceTwoAttack);
+        assertNotNull(dice2);
+        View dice3 = mActivity.findViewById(R.id.diceThreeAttack);
+        assertNotNull(dice3);
+        View dice4 = mActivity.findViewById(R.id.diceOneDefense);
+        assertNotNull(dice4);
+        View dice5 = mActivity.findViewById(R.id.diceTwoDefense);
+        assertNotNull(dice5);
+        View dice6 = mActivity.findViewById(R.id.diceThreeDefense);
+        assertNotNull(dice6);
+        View attackerLbl = mActivity.findViewById(R.id.attackerLbl);
+        assertNotNull(attackerLbl);
+        View defenderLbl = mActivity.findViewById(R.id.defenderLbl);
+        assertNotNull(defenderLbl);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+}
